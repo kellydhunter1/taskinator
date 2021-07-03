5082f5fe6d0e5e2ca8ec95b066e59549c55b56a9
kellydhunter1 <kellydhunter1@gmail.com>
2021-07-03 01:14:17 -0400

    refactored js and added reset after form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,51 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) { 
-	event.preventDefault();
+var createFormHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  // check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+      };
+    
+      // send it as an argument to createTaskEl
+      createTaskEl(taskDataObj);
+};
 
-formEl.addEventListener("submit", createTaskHandler)
+var createTaskEl = function(taskDataObj) {
+// create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
 
-// Add task to the list when “add task” button is clicked
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
 
-// Capture user form field values
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+listItemEl.appendChild(taskInfoEl);
 
-// reset form after user clicks “add task”
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+}
 
+formEl.addEventListener("submit", createFormHandler);
+
+// Rename the handler function to be a little more specific to the event it's handling.
+
+// Create a new function to take in the task's name and title as arguments and create the HTML elements that get added to the page.
+
+// Move the code that creates and adds HTML elements from the handler function into the newly created function.
+
+// Update the handler function to send the task name and type values from the form to the newly created function.
